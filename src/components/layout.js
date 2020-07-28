6182778f35bb4e2527ece03df1586386f8ce6f73
Svein Seldal <sveinse@seldal.com>
2020-07-28 03:15:01 +0200

    Commit component with bug

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,48 +1,101 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-import React from "react"
+import React, { Component } from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
+import { Menu, Responsive, Visibility, Sidebar, Icon } from "semantic-ui-react"
 
-import Header from "./header"
-import "./layout.css"
-
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
-  return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+const getWidth = () => {
+  const isSSR = typeof window === "undefined"
+  return isSSR ? Responsive.onlyTablet.minWidth : window.innerWidth
 }
+
+class DesktopContainer extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ fixed: false })
+  showFixedMenu = () => this.setState({ fixed: true })
+
+  render() {
+    const { fixed } = this.state
+
+    return (
+      <Responsive getWidth={getWidth} minWidth={Responsive.onlyTablet.minWidth}>
+        <Visibility
+          once={false}
+          onBottomPassed={this.showFixedMenu}
+          onBottomPassedReverse={this.hideFixedMenu}
+        >
+          <Menu fixed={fixed ? "top" : null}>
+            <Menu.Item as={Link} activeClassName="active" to="/">
+              Home
+            </Menu.Item>
+            <Menu.Item as={Link} activeClassName="active" to="/page-2">
+              Page2
+            </Menu.Item>
+          </Menu>
+        </Visibility>
+        {this.props.children}
+      </Responsive>
+    )
+  }
+}
+
+DesktopContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+class MobileContainer extends Component {
+  state = {}
+
+  handleSidebarHide = () => this.setState({ sidebarOpened: false })
+  handleToggle = () => this.setState({ sidebarOpened: true })
+
+  render() {
+    const { sidebarOpened } = this.state
+
+    return (
+      <Responsive
+        as={Sidebar.Pushable}
+        getWidth={getWidth}
+        maxWidth={Responsive.onlyMobile.maxWidth}
+      >
+        <Sidebar
+          as={Menu}
+          animation="push"
+          onHide={this.handleSidebarHide}
+          vertical
+          visible={sidebarOpened}
+        >
+          <Menu.Item as={Link} activeClassName="active" to="/">
+            Home
+          </Menu.Item>
+          <Menu.Item as={Link} activeClassName="active" to="/page-2">
+            Page2
+          </Menu.Item>
+        </Sidebar>
+
+        <Sidebar.Pusher dimmed={sidebarOpened}>
+          <Menu>
+            <Menu.Item onClick={this.handleToggle}>
+              <Icon name="sidebar" size="large" />
+            </Menu.Item>
+          </Menu>
+          {this.props.children}
+        </Sidebar.Pusher>
+      </Responsive>
+    )
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node,
+}
+
+const Layout = ({ children }) => (
+  <>
+    <DesktopContainer>{children}</DesktopContainer>
+    <MobileContainer>{children}</MobileContainer>
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,8 @@ const IndexPage = () => (
     </div>
     <Link to="/page-2/">Go to page 2</Link> <br />
     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    <div style={{ height: "600px" }} />
+    End
   </Layout>
 )
 
